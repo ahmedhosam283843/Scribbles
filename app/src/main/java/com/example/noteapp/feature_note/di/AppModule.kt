5476d0fa2db331b4126200dee4b436eb0e68b065
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-02-15 20:09:08 +0200

    feat: add/edit note viewmodel

--- app/src/main/java/com/example/noteapp/feature_note/di/AppModule.kt
+++ app/src/main/java/com/example/noteapp/feature_note/di/AppModule.kt
@@ -5,10 +5,7 @@ import androidx.room.Room
 import com.example.noteapp.feature_note.data.data_source.NoteDatabase
 import com.example.noteapp.feature_note.data.repository.NoteRepositoryImpl
 import com.example.noteapp.feature_note.domain.repository.NoteRepository
-import com.example.noteapp.feature_note.domain.use_case.AddNote
-import com.example.noteapp.feature_note.domain.use_case.DeleteNote
-import com.example.noteapp.feature_note.domain.use_case.GetNotes
-import com.example.noteapp.feature_note.domain.use_case.NoteUseCases
+import com.example.noteapp.feature_note.domain.use_case.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -48,6 +45,7 @@ object AppModule {
             getNotes = GetNotes(repository),
             deleteNote = DeleteNote(repository),
             addNote = AddNote(repository),
+            getNote = GetNote(repository)
         )
     }
 
